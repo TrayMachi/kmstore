e8c805a30b1932bac5041c0fbf911c396abd5f34
Tristan A. <tristanagraa@gmail.com>
2024-09-10 02:31:55 +0700

    feat: Add initial models for keyboard and mouse

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class keyboard(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.IntegerField()
+    switch = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
+    
+class mouse(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.IntegerField()
+    dpi = models.IntegerField()
+    weight = models.IntegerField()
+    brand = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name

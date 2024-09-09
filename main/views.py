b4b7a9ecf6aea1dc002559d9a5cd63db56eb6269
Tristan A. <tristanagraa@gmail.com>
2024-09-10 02:32:12 +0700

    feat: Add show_main view with context data

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'title': 'KMStore',
+        'name': 'Tristan Agra Yudhistira',
+        'npm' : '2306245112',
+        'class': 'PBP D'
+    }
+
+    return render(request, "main.html", context)

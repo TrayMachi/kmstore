e47812a319ff3122623bf68288db194792234b32
Tristan A. <tristanagraa@gmail.com>
2024-09-11 23:49:52 +0700

    Refactor: Update models.py to include UUIDField and created_at field

--- main/models.py
+++ main/models.py
@@ -1,27 +1,34 @@
 from django.db import models
+import uuid
+
 
 # Create your models here.
 class Keyboard(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
     switch = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
+    image = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-    
+
+
 class Mouse(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField()
     dpi = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
     weight = models.IntegerField()
     brand = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
+    image = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name

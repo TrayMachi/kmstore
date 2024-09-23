924185eacd2fd22d116f88052f0b9a84043007ba
Tristan A. <tristanagraa@gmail.com>
2024-09-23 21:29:23 +0700

    feat: complete auth and cookies

--- main/urls.py
+++ main/urls.py
@@ -11,20 +11,34 @@ from main.views import (
     show_xml_by_id_mouse,
     show_json_by_id_keyboard,
     show_json_by_id_mouse,
+    register,
+    login_user,
+    logout_user,
 )
 
-app_name = 'main'
+app_name = "main"
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
-    path('create/keyboard/', create_keyboard, name='create_keyboard'),
-    path('create/mouse/', create_mouse, name='create_mouse'),
-    path('xml/keyboard/', show_xml_keyboard, name='show_xml_keyboard'),
-    path('xml/mouse/', show_xml_mouse, name='show_xml_mouse'),
-    path('json/keyboard/', show_json_keyboard, name='show_json_keyboard'),
-    path('json/mouse/', show_json_mouse, name='show_json_mouse'),
-    path('xml/keyboard/<str:id>/', show_xml_by_id_keyboard, name='show_xml_by_id_keyboard'),
-    path('xml/mouse/<str:id>/', show_xml_by_id_mouse, name='show_xml_by_id_mouse'),
-    path('json/keyboard/<str:id>/', show_json_by_id_keyboard, name='show_json_by_id_keyboard'),
-    path('json/mouse/<str:id>/', show_json_by_id_mouse, name='show_json_by_id_mouse'),
+    path("", show_main, name="show_main"),
+    path("create/keyboard/", create_keyboard, name="create_keyboard"),
+    path("create/mouse/", create_mouse, name="create_mouse"),
+    path("xml/keyboard/", show_xml_keyboard, name="show_xml_keyboard"),
+    path("xml/mouse/", show_xml_mouse, name="show_xml_mouse"),
+    path("json/keyboard/", show_json_keyboard, name="show_json_keyboard"),
+    path("json/mouse/", show_json_mouse, name="show_json_mouse"),
+    path(
+        "xml/keyboard/<str:id>/",
+        show_xml_by_id_keyboard,
+        name="show_xml_by_id_keyboard",
+    ),
+    path("xml/mouse/<str:id>/", show_xml_by_id_mouse, name="show_xml_by_id_mouse"),
+    path(
+        "json/keyboard/<str:id>/",
+        show_json_by_id_keyboard,
+        name="show_json_by_id_keyboard",
+    ),
+    path("json/mouse/<str:id>/", show_json_by_id_mouse, name="show_json_by_id_mouse"),
+    path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,68 +1,141 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from main.models import Keyboard, Mouse
 from main.forms import KeyboardForm, MouseForm
 from django.core import serializers
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.urls import reverse
+
 
 # Create your views here.
+@login_required(login_url="/login")
 def show_main(request):
     context = {
-        'title': 'KMStore',
-        'name': 'Tristan Agra Yudhistira',
-        'npm' : '2306245112',
-        'class': 'PBP D',
+        "title": "KMStore",
+        "name": "Tristan Agra Yudhistira",
+        "npm": "2306245112",
+        "class": "PBP D",
         "keyboards": Keyboard.objects.all(),
         "mouse": Mouse.objects.all(),
     }
 
     return render(request, "main.html", context)
 
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been successfully created!")
+            return redirect("main:login")
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie("last_login", str(datetime.datetime.now()))
+            return response
+
+    else:
+        form = AuthenticationForm(request)
+    context = {"form": form}
+    return render(request, "login.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+
+@login_required(login_url="/login")
 def create_keyboard(request):
     form = KeyboardForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
         form.save()
         return redirect("main:show_main")
-    
-    return render(request, "keyboard_form.html", {"form": form}) 
 
+    return render(request, "keyboard_form.html", {"form": form})
+
+
+@login_required(login_url="/login")
 def create_mouse(request):
     form = MouseForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
         form.save()
         return redirect("main:show_main")
-    
+
     return render(request, "mouse_form.html", {"form": form})
+
 
 def show_xml_keyboard(request):
     data = Keyboard.objects.all()
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
 
 def show_xml_mouse(request):
     data = Mouse.objects.all()
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
 
 def show_json_keyboard(request):
     data = Keyboard.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
 
 def show_json_mouse(request):
     data = Mouse.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
 
 def show_xml_by_id_keyboard(request, id):
     data = Keyboard.objects.get(id=id)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
 
 def show_xml_by_id_mouse(request, id):
     data = Mouse.objects.get(id=id)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
 
 def show_json_by_id_keyboard(request, id):
     data = Keyboard.objects.get(id=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
 
 def show_json_by_id_mouse(request, id):
     data = Mouse.objects.get(id=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
